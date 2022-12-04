de54473a91183da2fec307e46d98dd24add06fba
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2022-12-04 13:39:40 -0500

    Added FPS display
    FPS display shows red text if below cutoff threshold (intentionally high for now until we can notice substantial FPS drops). Added render delay so that FPS is only recalculated every 0.2 seconds

--- Assets/Scripts/ProbingTextUpdater.cs
+++ Assets/Scripts/ProbingTextUpdater.cs
@@ -5,12 +5,22 @@ using TMPro;
 
 public class ProbingTextUpdater : MonoBehaviour
 {
+    // constants
+    private const int redCutoff = 500;
+    private const float delay = 0.2f;
+
+    // variables
+    private float delayTimer;
+
     // Unity variables
     private TextMeshProUGUI text;
 
     // Start is called before the first frame update
     void Start()
     {
+        // variables
+        delayTimer = delay+0.01f; // ensures automatic text setting in first frame
+
         // Unity variables
         text = GetComponent<TextMeshProUGUI>();
     }
@@ -18,6 +28,16 @@ public class ProbingTextUpdater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.SetText("Probing Method:\n" + (GameManager.instance.getIsLinear()?"Linear":"Quadratic"));
+        if(delayTimer > delay)
+        {
+            // render probing method stored in game manager and make FPS red if below cutoff threshold
+            text.SetText("<u>Probing Method</u>:\n" + (GameManager.instance.getIsLinear() ? "Linear" : "Quadratic")
+            + "\n<u>FPS</u>: " + ((int)(1.0f / Time.deltaTime) < redCutoff ? "<color=red>" + ((int)(1.0f / Time.deltaTime)).ToString() + "</color>" : (int)(1.0f / Time.deltaTime)));
+
+            delayTimer = 0;
+        }
+        
+
+        delayTimer += Time.deltaTime;
     }
 }
