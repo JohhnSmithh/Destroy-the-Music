ad362ad145df404953cefb60fc42f02659bbda19
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2022-12-04 19:32:17 -0500

    Basic true/false question loop

--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class QuestionManager : MonoBehaviour
 {
@@ -14,11 +15,11 @@ public class QuestionManager : MonoBehaviour
     private const int MAX_HP = 100;
     private const int HP_ON_CORRECT = 10;
     private const int HP_ON_INCORRECT = 5;
+    private const float NEXT_QUESTION_DELAY = 5f;
 
     // variables
     private enum QuestionType
     {
-        None,
         TrueFalse,
         MultipleChoiceArtist,
         MultipleChoiceAlbum,
@@ -27,38 +28,96 @@ public class QuestionManager : MonoBehaviour
     private QuestionType questionType;
     private float correctAnswer;
     private int bossHP = 100; // will be replaced by hash map size once implemented
+    private bool answered;
+    private float nextQuestionTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        questionType = QuestionType.TrueFalse; // temporary
+        // variables
+        answered = true;
+        nextQuestionTimer = -5; // creates additional 5 second delay on first dialogue
 
         // initialize all buttons to NOT active
         trueButton.SetActive(false);
         falseButton.SetActive(false);
+
+        // set up text/dialogue for before any questions
+        questionText.SetText("Welcome music haters to the most popular game show of all time, Destroy The Music! " +
+            "Let's see what you've got as we move on to your first challenging question!");
     }
 
     // Update is called once per frame 
     void Update()
     {
-        if(questionType == QuestionType.None)
+        if(answered)
         {
-            // put transition here between starting and beginning questions -> with some delay mechanic
+            nextQuestionTimer += Time.deltaTime;
+
+            // load next random questiona after timer delay
+            if(nextQuestionTimer > NEXT_QUESTION_DELAY)
+            {
+                // randomly select next question type
+                questionType = (QuestionType) Random.Range(0, 1);
+
+                if (questionType == QuestionType.TrueFalse)
+                {
+                    // Set Question Text
+                    questionText.SetText("True or False? The following song is explicit: ");
+
+                    // 1 indicates true, 0 indicates false
+                    correctAnswer = 1; // replace with reading from random song from hash map object
+
+                    // activate true/false buttons and disable all other UI
+                    trueButton.SetActive(true);
+                    falseButton.SetActive(true);
+                    trueButton.GetComponent<Button>().interactable = true;
+                    falseButton.GetComponent<Button>().interactable = true;
+                    trueButton.transform.GetChild(1).gameObject.SetActive(false);
+                    trueButton.transform.GetChild(2).gameObject.SetActive(false);
+                    falseButton.transform.GetChild(1).gameObject.SetActive(false);
+                    falseButton.transform.GetChild(2).gameObject.SetActive(false);
+
+                }
+                else if(questionType == QuestionType.MultipleChoiceAlbum)
+                {
+                    // Set Question Text
+                    questionText.SetText("What is the Album name for the following song: ");
+
+                    // 0 is a, 1 is b, 2 is c, and 3 is d
+                    correctAnswer = 0;
+
+                    // activate multiple choice buttons and disable all other UI
+                    trueButton.SetActive(false);
+                    falseButton.SetActive(false);
+                }
+                else if(questionType == QuestionType.MultipleChoiceArtist)
+                {
+                    // Set Question Text
+                    questionText.SetText("What is the Artist name for the following song: ");
+
+                    // 0 is a, 1 is b, 2 is c, and 3 is d
+                    correctAnswer = 0;
+
+                    // activate multiple choice buttons and disable all other UI
+                    trueButton.SetActive(false);
+                    falseButton.SetActive(false);
+                }
+                else if(questionType == QuestionType.DaneabilitySlider)
+                {
+                    // Set Question Text
+                    questionText.SetText("Make your best guess at the danceability of the following song: ");
+
+                    correctAnswer = 0.5f;
+
+                    // activate confidence slider and disable all other UI
+                    trueButton.SetActive(false);
+                    falseButton.SetActive(false);
+                }
+
+                answered = false;
+            }
         }
-        if(questionType == QuestionType.TrueFalse)
-        {
-            // Set Question Text
-            questionText.SetText("True or False? The following song is explicit: ");
-
-            // 1 indicates true, 0 indicates false
-            correctAnswer = 1; // replace with reading from hash map object for actual correct answer
-
-            // activate true/false buttons and disable all other UI
-            trueButton.SetActive(true);
-            falseButton.SetActive(true);
-        }
-
-        // add boss HP bar render
     }
 
     public float getHealthPercentage()
@@ -69,23 +128,22 @@ public class QuestionManager : MonoBehaviour
     #region BUTTON PRESS FUNCTIONS
     public void truePress()
     {
-        if (correctAnswer == 1)
-        {
-            bossHP -= HP_ON_CORRECT;
-            if (bossHP < 0)
-                bossHP = 0;
-        }
-        else
-        {
-            bossHP += HP_ON_INCORRECT;
-            if (bossHP > MAX_HP)
-                bossHP = MAX_HP; // ensures HP does not exceed max
-        }
+        trueFalsePress(true);
     }
 
     public void falsePress()
     {
-        if (correctAnswer == 0)
+        trueFalsePress(false);
+    }
+
+    private void trueFalsePress(bool buttonType)
+    {
+        answered = true;
+        nextQuestionTimer = 0;
+        trueButton.GetComponent<Button>().interactable = false;
+        falseButton.GetComponent<Button>().interactable = false;
+
+        if (correctAnswer == (buttonType ? 1 : 0))
         {
             bossHP -= HP_ON_CORRECT;
             if (bossHP < 0)
@@ -96,6 +154,18 @@ public class QuestionManager : MonoBehaviour
             bossHP += HP_ON_INCORRECT;
             if (bossHP > MAX_HP)
                 bossHP = MAX_HP; // ensures HP does not exceed max
+        }
+
+        // show check and x marks to show correct and incorrect answers
+        if (correctAnswer == 1)
+        {
+            trueButton.transform.GetChild(2).gameObject.SetActive(true);
+            falseButton.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            trueButton.transform.GetChild(1).gameObject.SetActive(true);
+            falseButton.transform.GetChild(2).gameObject.SetActive(true);
         }
     }
     #endregion
